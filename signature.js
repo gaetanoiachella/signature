e424df397bf997537b324756fb0d8bf2a873d5a8
Gaetano Iachella <g.iachella@gmail.com>
2014-07-14 16:45:16 +0200

    add relationship attribute

--- signature.js
+++ signature.js
@@ -1,2 +1,2 @@
 document.write('Powered by <a style="color: #888;" href="http://rivaweb.it">Studio AR. </a>');
-document.write('Developed by <a style="color: #888;" href="http://gaetanoiachella.it">Gaetano Iachella. </a>');
+document.write('Developed by <a style="color: #888;"  rel="author" href="http://gaetanoiachella.it">Gaetano Iachella. </a>');
