c9ce8afbb7004acdf2711ed40ff6029984bb3f03
Gaetano Iachella <g.iachella@gmail.com>
2014-07-14 13:47:57 +0200

    Fix color at link

--- signature.js
+++ signature.js
@@ -1,2 +1,2 @@
-document.write('Powered by <a href="http://rivaweb.it">Studio AR.</a>');
-document.write('Developed by <a href="http://gaetanoiachella.it">Gaetano Iachella.</a>');
+document.write('Powered by <a style="color: #888;" href="http://rivaweb.it">Studio AR. </a>');
+document.write('Developed by <a style="color: #888;" href="http://gaetanoiachella.it">Gaetano Iachella. </a>');
