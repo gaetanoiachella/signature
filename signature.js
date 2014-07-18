eb6c9818d8eef542d1744bb53939d1c1dae62d35
Gaetano Iachella <g.iachella@gmail.com>
2014-07-18 13:56:47 +0200

    Update signature.js

--- signature.js
+++ signature.js
@@ -1,2 +1,2 @@
-document.write('Powered by <a style="color: #888;" target="_blank" href="http://rivaweb.it">Studio AR. </a>');
+document.write('Powered by <a style="color: #888;" target="_blank" href="http://rivaweb.it">Studio AR.</a> <br />');
 document.write('Developed by <a style="color: #888;" target="_blank"  rel="author" href="http://gaetanoiachella.it">Gaetano Iachella. </a>');
